275eecf11c12cde953790222295ba1e6d1552eed
MrTimscampi <julien.machiels@protonmail.com>
2021-08-31 21:53:28 +0200

    feat(app): add logging

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,63 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use fern::colors::{Color, ColoredLevelConfig};
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    let colors_level = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Blue)
+        .debug(Color::White)
+        .trace(Color::Magenta);
+
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Debug)
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, record| {
+                    out.finish(format_args!(
+                        "[{}][{}][{}] {}",
+                        chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                        record.target(),
+                        colors_level.color(record.level()),
+                        message
+                    ))
+                })
+                .chain(std::io::stdout())
+            )
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, record| {
+                    out.finish(format_args!(
+                        "[{}][{}][{}] {}",
+                        chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                        record.target(),
+                        record.level(),
+                        message
+                    ))
+                })
+                .chain(fern::DateBased::new("meiti.server.", "%Y-%m-%d.log"))
+            )
+        .apply()?;
+    Ok(())
+}
+
+#[get("/healh")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    setup_logger().expect("failed to initialize logging.");
+
+    log::info!("Meiti Media Server v{}", VERSION);
+
     HttpServer::new(|| {
-        App::new().service(
-            // prefixes all resources and routes attached to it...
-            web::scope("/app")
-                // ...so this handles requests for `GET /app/index.html`
-                .route("/", web::get().to(index)),
-        )
+        App::new()
+            .service(health)
     })
     .bind("127.0.0.1:23400")?
     .run()
