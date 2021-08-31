40d6344799fc47ce4a62a6548444135028c635bb
MrTimscampi <julien.machiels@protonmail.com>
2021-08-31 20:33:19 +0200

    feat: add basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/app")
+                // ...so this handles requests for `GET /app/index.html`
+                .route("/", web::get().to(index)),
+        )
+    })
+    .bind("127.0.0.1:23400")?
+    .run()
+    .await
 }
